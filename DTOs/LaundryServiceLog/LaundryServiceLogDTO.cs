173a0210ac51c062ba8329553bf9ec00f4422df7
MallorcaTheGreat <gianphilipmallorca@dmc.edu.ph>
2024-11-14 19:21:46 +0800

    updatesssss

--- DTOs/LaundryServiceLog/LaundryServiceLogDTO.cs
+++ DTOs/LaundryServiceLog/LaundryServiceLogDTO.cs
@@ -5,6 +5,7 @@
         public Guid LaundryServiceLogId { get; set; }
         public Guid LaundryShopId { get; set; }
         public List<Guid> ServiceIds { get; set; }
+        public bool IsActive { get; set; }
         public string? ServiceDescription { get; set; }
         public decimal? Price { get; set; }
     }
